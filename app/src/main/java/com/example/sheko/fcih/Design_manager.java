057f00e1dff671e0b3bf2fbe5b7b2184cc025118
shrief-mohamed-ramadan <shriefmarcato@gmail.com>
2018-04-19 15:37:06 +0200

    initial

---
+++ app/src/main/java/com/example/sheko/fcih/Design_manager.java
@@ -0,0 +1,43 @@
+package com.example.sheko.fcih;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+/**
+ * Created by Sheko on 4/18/2018.
+ */
+
+//Class for managing Desing (create table - frame - ...)
+
+public class Design_manager {
+
+    /*
+     * Function: Generate_Table
+     * ----------------------------
+     *   Function for generating table
+     *   @Paramters
+     *              Resultset from database contatins data
+     *              TableLayout for display data on it
+     *
+     *   @returns: void (nothing)
+     */
+    public static void Generate_Table (ResultSet data , TableLayout table , Context app_context) {
+        try
+        {
+
+        }//end try
+        catch (Exception e)
+        {
+
+        }//end catch
+    }//end function Generate_Table
+}//end Class Design_manager
---
+++ app/src/main/java/com/example/sheko/fcih/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.sheko.fcih;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.Toast;
+
+import java.sql.ResultSet;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+}
